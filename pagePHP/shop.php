f3ce61678c327e7f299623cad6093e0fb9a08b8b
SenuinLentar <myal64@free.fr>
2018-04-13 08:54:12 +0200

    fiew changes on the shop

--- pagePHP/shop.php
+++ pagePHP/shop.php
@@ -30,36 +30,46 @@
                 while ($data = $answer->fetch())
                 {
             ?>
-                    <img src="<?php echo $data['URL']; ?>" alt="<?php echo $data['NameGoodies']; ?>" title="<?php echo $data['NameGoodies']; ?>" class="shop-picture"/>
+                    <div class="shop">
+                        <img src="<?php echo $data['URL']; ?>" alt="<?php echo $data['NameGoodies']; ?>" title="<?php echo $data['NameGoodies']; ?>" class="shop-picture"/>
 
-                    <div class="goodies_information invisible">
-                        <div class="goodies_information_part">
+                        <div class="goodies_information invisible">
+                            <div class="goodies_information_part">
 
-                            <img src="<?php echo $data['URL']; ?>" alt="<?php echo $data['NameGoodies']; ?>" title="<?php echo $data['NameGoodies']; ?>" class="goodies-picture"/>
+                                <img src="<?php echo $data['URL']; ?>" alt="<?php echo $data['NameGoodies']; ?>" title="<?php echo $data['NameGoodies']; ?>" class="goodies-picture"/>
 
-                            <div class="info_goodies info_goodies_margin">Nom :<br/><?php echo $data['NameGoodies']; ?></div>
+                                <div class="info_goodies">Nom :<br/><?php echo $data['NameGoodies']; ?></div>
 
-                            <div class="info_goodies info_goodies_margin">Catégorie :<br/><?php echo $data['NameGoodiesCategory']; ?></div>
 
-                            <div class="info_goodies info_goodies_margin">Prix :<br/><?php echo $data['Price']; ?>€</div>
+                                <div class="info_goodies">Catégorie :<br/><?php echo $data['NameGoodiesCategory']; ?></div>
 
+                                <div class="info_goodies">Prix :<br/><?php echo $data['Price']; ?>€</div>
+
+                            </div>
+                            <div class="goodies_information_part">
+
+                                <div class="info_goodies info_goodie_description info_goodies_margin">Description :<br/><?php echo $data['Description']; ?></div>
+
+                            </div>
+                            <div class="goodies_information_part goodies_information_part_position">
+
+                                <div class="info_goodies">acheter</div>
+                                <div class="info_goodies">supprimer</div>
+
+                            </div>
                         </div>
-                        <div class="goodies_information_part">
-
-                            <div class="info_goodies info_goodie_description info_goodies_margin">Description :<br/><?php echo $data['Description']; ?></div>
-
-                        </div>
-                        <div class="goodies_information_part goodies_information_part_position">
-
-                            <div class="info_goodies">acheter</div>
-                            <div class="info_goodies">supprimer</div>
-
-                        </div>
-                    </div>
-
+                </div>
             <?php
                 }
             ?>
+            <div id="sidebar">
+                <input type="text" placeholder="Recherche"/>
+                <p>Filtres :</p>
+                <p>Catégories</p>
+                <p>Prix</p>
+                <p>Popularité</p>
+                <p>Ajouter</p>
+            </div>
         </section>
 
         <footer id="bas">
