f7cf3bd378e012dc5b898f0bdb5425eb7a86d9ea
Emma Laroudie <laroudie.e@gmail.com>
2018-04-16 09:00:37 +0200

    Nav fixed

--- pagePHP/nav.php
+++ pagePHP/nav.php
@@ -1,7 +1,3 @@
-<?php
-include ('modalInscription.php');
-include ('modalLogin.php');
-?>
 <link rel="stylesheet" href="/projetWeb/feuilleCSS/style-nav.css">
 <link rel="stylesheet" href="/projetWeb/feuilleCSS/style-dropdownMenu.css">
 
@@ -21,28 +17,9 @@ include ('modalLogin.php');
             <?php
             }
             else {
+                include ('modalInscription.php');
+                include ('modalLogin.php');
             ?>
-
-<div>
-    <link rel="stylesheet" href="/projetWeb/feuilleCSS/style-nav.css">
-    <img id="exia" src="/projetWeb/imagePNG/exia.png" alt="logo exia">
-    <nav class="table">
-        <a href="home.php" class="bouton"> <li> Accueil </li> </a>
-        <a href="shop.php" class="bouton"> <li> Boutique </li> </a>
-        <a href="#" class="bouton"> <li> Boite à idées </li> </a>
-        <a href="eventOfTheMonth.php" class="bouton"> <li> Évènement du mois </li> </a>
-        <a href="#" class="bouton"> <li> Évènements passés </li> </a>
-        <?php
-        if(isset($_SESSION['Login'])){
-        ?>
-            <a href="/projetWeb/pagePHP/destroySession.php" class="bouton"><li>Déconnexion</li></a>
-        <?php
-        }
-        else {
-include ('modalInscription.php');
-include ('modalLogin.php');
-        ?>
->>>>>>> origin/master
             <button id ="authentificationBtn" onclick="document.getElementById('id01').style.display='block'">
                 <img src="/projetWeb/imagePNG/signIn.png" alt="signIn ">
             </button>
@@ -67,9 +44,9 @@ include ('modalLogin.php');
 <div class="dropdown">
     <button onclick="myFunction()" class="dropbtn">
         menu
-<!--                <img src="/projetWeb/imagePNG/Menu_icon.png" alt="menu">-->
-</button>
-<!--    <input type="button" onclick="myFunction()" class="dropbtn" src="/projetWeb/imagePNG/Menu_icon.png"/>-->
+<!--                        <img src="/projetWeb/imagePNG/Menu_icon.png" alt="menu">-->
+    </button>
+    <!--    <input type="button" onclick="myFunction()" class="dropbtn" src="/projetWeb/imagePNG/Menu_icon.png"/>-->
     <div id="myDropdown" class="dropdown-content">
         <a href="home.php">Accueil</a>
         <a href="shop.php">Boutique</a>
