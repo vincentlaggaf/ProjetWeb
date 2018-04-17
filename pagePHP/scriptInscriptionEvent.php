66a47ba324f4e649808614c3a20adef9f668731e
Emma Laroudie <laroudie.e@gmail.com>
2018-04-17 09:47:53 +0200

    pagePHP/scriptInscriptionEvent.php cleaned

--- pagePHP/scriptInscriptionEvent.php
+++ pagePHP/scriptInscriptionEvent.php
@@ -28,61 +28,36 @@ if(isset($_SESSION['Id']) AND isset($IDEvent)){
     $IDUserFromBDD=$check->fetch();
     echo $IDUser;
 
-   // echo $IDUserFromBDD['IDUser'];
+    // echo $IDUserFromBDD['IDUser'];
 
-        if($IDUser==$IDUserFromBDD['IDUser'])
-        {
-
+    if($IDUser==$IDUserFromBDD['IDUser'])
+    {
         echo "<script>alert('Vous êtes déjà inscrit à l\'évènement !');
-<<<<<<< HEAD
-        window.location.replace('eventOfTheMonth.php');</script>";
+            window.location.replace('eventOfTheMonth.php');</script>";
     }
     else {
-        $addUserToEvent=$bdd->prepare("INSERT INTO interest (IDUser,Participate,IDEvent) VALUES(4,1,:IDEvent)");
+        $addUserToEvent=$bdd->prepare("INSERT INTO Interest (IDUser,Participate,IDEvent) VALUES(:IDUser,1,:IDEvent)");
         $addUserToEvent->bindValue(':IDEvent',$IDEvent,PDO::PARAM_INT);
+        $addUserToEvent->bindValue(':IDUser',$IDUser,PDO::PARAM_INT);
         $addUserToEvent->execute();
-        echo "Inscription réussie";
+        header('Location:http://localhost/projetWeb/pagePHP/pageOfEvent.php?name='.$EventPath);
     }
-=======
-            window.location.replace('eventOfTheMonth.php');</script>";
-
-        }
-        else {
-            $addUserToEvent=$bdd->prepare("INSERT INTO Interest (IDUser,Participate,IDEvent) VALUES(:IDUser,1,:IDEvent)");
-
-            $addUserToEvent->bindValue(':IDEvent',$IDEvent,PDO::PARAM_INT);
-            $addUserToEvent->bindValue(':IDUser',$IDUser,PDO::PARAM_INT);
-            $addUserToEvent->execute();
-            header('Location:http://localhost/projetWeb/pagePHP/pageOfEvent.php?name='.$EventPath);
-
-        }
->>>>>>> origin/master
-
-
-        }
-        else{
-
-<<<<<<< HEAD
-    header('Location: /projetWeb/pagePHP/eventOfTheMonth.php');
-    exit();
+}
+else{
+    echo "<script>alert('Vous n\'êtes pas connectés !');</script>";
+    echo '<script> document.location.replace(/projetWeb/pagePHP/eventOfTheMonth.php);</script>';
+    //        header('Location: /projetWeb/pagePHP/eventOfTheMonth.php');
+    //    exit();
 
     //    include ('modalInscription.php');
     //    document.getElementById('id01').style.display='block';
     // include('modalLogin.php');
-=======
-        //
-        //    include ('modalInscription.php');
-        //    document.getElementById('id01').style.display='block';
 
-         //include('modalLogin.php');
+    //
+    //    include ('modalInscription.php');
+    //    document.getElementById('id01').style.display='block';
 
-
-        echo "<script>alert('Vous n\'êtes pas connectés !');
-                ;</script>";
-           include ('modalInscription.php');
-             "<script>    document.getElementById('id01').style.display='block';</script>";
-
->>>>>>> origin/master
+    //include('modalLogin.php');
 
     //echo '<script> document.location.replace(window.history.back());</script>';
     //    echo "<script>alert('Vous n\'êtes pas connectés !');</script>";
@@ -91,16 +66,12 @@ if(isset($_SESSION['Id']) AND isset($IDEvent)){
     //include ('modalInscription.php');
     //     "<script> document.getElementById('id01').style.display='block';</script>";
     //"<script> document.getElementById('id01').style.display='block';</script>"
+    //  header('Location:http://localhost/projetWeb/pagePHP/eventOfTheMonth.php');
 
-
-
-<<<<<<< HEAD
-    //
 }
-=======
 
-           //  header('Location:http://localhost/projetWeb/pagePHP/eventOfTheMonth.php');
-        }
->>>>>>> origin/master
+
+
+
 
 ?>
